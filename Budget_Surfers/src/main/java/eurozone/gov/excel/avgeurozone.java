5752304a0cadb6d0cd83ed18085f97f318e782e6
manolisfrai <t8240161@aueb.gr>
2025-11-13 13:24:56 +0200

    Correct the bounds of the array and the declaration method of arrays and changing the type of GTP to long because of its size

--- Budget_Surfers/src/main/java/eurozone/gov/excel/avgeurozone.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/avgeurozone.java
@@ -7,18 +7,18 @@ public class avgeurozone {
         }
         return grministr;
     }
-    public static double ministrDiv(String[][] grministr) {
-        final int GTP = 257100000000;
-        int publicserv = grministr[4] + grministr[20] + grministr[5] + grministr[11];
-        int def = grministr[6];
-        int pubordsaf = grministr[22] + grministr[8] + (int) 0.78 * grministr[23];
-        int econ = grministr[16] + (int) 0.80 * grministr[17] + grministr[12] + grministr[19] + grministr[18] + (int) 0.37 * grministr[13];
-        int envprot = (int) 0.41 * grministr[13] + (int) 0.22 * grministr[23];
-        int houscomamen = (int) 0.20 * grministr[17] + (int) 0.22 * grministr[13];
-        int health = grministr[7];
-        int recrculrel = grministr[10] + (int) 0.06 * grministr[9];
-        int edu = (int) 0.94 * grministr[9];
-        int socialprot = grministr[14] + grministr[15] + grministr[21];
+    public static double ministrDiv(int[] grministr) {
+        final long budget = 257100000000L;
+        int publicserv = grministr[0] + grministr[16] + grministr[1] + grministr[7];
+        int def = grministr[2];
+        int pubordsaf = grministr[18] + grministr[4] + (int) 0.78 * grministr[19];
+        int econ = grministr[12] + (int) 0.80 * grministr[13] + grministr[8] + grministr[15] + grministr[14] + (int) 0.37 * grministr[9];
+        int envprot = (int) 0.41 * grministr[9] + (int) 0.22 * grministr[19];
+        int houscomamen = (int) 0.20 * grministr[13] + (int) 0.22 * grministr[9];
+        int health = grministr[3];
+        int recrculrel = grministr[6] + (int) 0.06 * grministr[5];
+        int edu = (int) 0.94 * grministr[5];
+        int socialprot = grministr[10] + grministr[11] + grministr[17];
         int grsum = publicserv + def + pubordsaf + econ + envprot + houscomamen + health + recrculrel + edu + socialprot;
         int [] sectors = {publicserv, def, pubordsaf, econ, envprot, houscomamen, health, recrculrel, edu, socialprot, sum};
         double [] grpercent = new double[11];
@@ -28,8 +28,8 @@ public class avgeurozone {
         return grpercent;
     }
     public static double compareGrToEurozone(double[] grpercent) {
-        double euzpercent = {6.0, 1.2, 1.7, 5.7, 0.9, 1.2, 7.4, 1.1, 4.6, 19.8, 49.5};
-        double diffgreuz = new double[11];
+        double [] euzpercent = {6.0, 1.2, 1.7, 5.7, 0.9, 1.2, 7.4, 1.1, 4.6, 19.8, 49.5};
+        double [] diffgreuz = new double[11];
         for(int i = 0; i<11; i++) {
             diffgreuz[i] = euzpercent[i] - grpercent[i];
         }
