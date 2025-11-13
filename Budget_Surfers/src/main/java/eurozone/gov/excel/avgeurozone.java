c688c4280fa6a43f9f39398e54c1af575eb658f8
despina petrogianni <t8240113@aueb.gr>
2025-11-13 12:11:18 +0200

    Correct table to have int numbers

--- Budget_Surfers/src/main/java/eurozone/gov/excel/avgeurozone.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/avgeurozone.java
@@ -1,17 +1,24 @@
 package eurozone.gov.excel;
 public class avgeurozone {
-    public static double ministrDiv(String[][] budget) {
+    public static int[] converToInt(String[][] budget) {
+        int[] grministr = new int[20];
+        for (int i = 4; i < 24; i++) {
+            grministr[i-4] = Integer.parseInt(budget[i][2]);
+        }
+        return grministr;
+    }
+    public static double ministrDiv(String[][] grministr) {
         final int GTP = 257100000000;
-        int publicserv = budget[4][2] + budget[20][2] + budget[5][2] + budget[11][2];
-        int def = budget[6][2];
-        int pubordsaf = budget[22][2] + budget[8][2] + (int) 0.78 * budget[23][2];
-        int econ = budget[16][2] + (int) 0.80 * budget[17][2] + budget[12][2] + budget[19][2] + budget[18][2] + (int) 0.37 * budget[13][2];
-        int envprot = (int) 0.41 * budget[13][2] + (int) 0.22 * budget[23][2];
-        int houscomamen = (int) 0.20 * budget[17][2] + (int) 0.22 * budget[13][2];
-        int health = budget[7][2];
-        int recrculrel = budget[10][2] + (int) 0.06 * budget[9][2];
-        int edu = (int) 0.94 * budget[9][2];
-        int socialprot = budget[14][2] + budget[15][2] + budget[21][2];
+        int publicserv = grministr[4] + grministr[20] + grministr[5] + grministr[11];
+        int def = grministr[6];
+        int pubordsaf = grministr[22] + grministr[8] + (int) 0.78 * grministr[23];
+        int econ = grministr[16] + (int) 0.80 * grministr[17] + grministr[12] + grministr[19] + grministr[18] + (int) 0.37 * grministr[13];
+        int envprot = (int) 0.41 * grministr[13] + (int) 0.22 * grministr[23];
+        int houscomamen = (int) 0.20 * grministr[17] + (int) 0.22 * grministr[13];
+        int health = grministr[7];
+        int recrculrel = grministr[10] + (int) 0.06 * grministr[9];
+        int edu = (int) 0.94 * grministr[9];
+        int socialprot = grministr[14] + grministr[15] + grministr[21];
         int grsum = publicserv + def + pubordsaf + econ + envprot + houscomamen + health + recrculrel + edu + socialprot;
         int [] sectors = {publicserv, def, pubordsaf, econ, envprot, houscomamen, health, recrculrel, edu, socialprot, sum};
         double [] grpercent = new double[11];
