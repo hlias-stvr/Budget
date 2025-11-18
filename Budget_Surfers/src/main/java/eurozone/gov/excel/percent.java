0b74a7ae450240f2be23006ee7d7f002ded40eba
hlias-stvr <t8240247@aueb.gr>
2025-11-18 21:56:12 +0200

    Changes in amount index for and added in package

--- Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
@@ -1,3 +1,5 @@
+package eurozone.gov.excel;
+    
 public class percent {
     public static long converterToLong(String[][] revenue,int k, int a) { // k how many lines and a the line of excel where the copy of the other array begin 
         long[][] dataToLong = new long[k][5];
@@ -20,13 +22,12 @@ public class percent {
         return variance;
     }
     public static double[] amount(long[][] LongData) {
-        double[][] amountdiff = new double[32][5];
-            for (int i = 0; i < 32; i++) {
-                for (int j = 0; j < 5  ; j++) {
+        double[][] amountdiff = new double[LongData.length][4];
+            for (int i = 0; i < amountdiff.length ; i++) {
+                for (int j = 0; j < 4  ; j++) {
                     amountdiff[i][j] = (LongData[i][j] - LongData[i][j+1]);
                 }
             }
         return amountdiff;
-        
     }
 }
