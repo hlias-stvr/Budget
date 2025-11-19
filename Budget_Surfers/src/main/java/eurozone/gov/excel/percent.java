f7b91e906a110f6c5a9422d840c603ad36aea6b1
xrhstosaltanhs <t8240249@aueb.gr>
2025-11-19 16:31:39 +0200

    Correct some syntax mistakes and one logic in the first loop

--- Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
@@ -1,18 +1,17 @@
 package eurozone.gov.excel;
     
 public class percent {
-    public static long converterToLong(String[][] revenue,int k, int a) { // k how many lines and a the line of excel where the copy of the other array begin 
+    public static long[][] converterToLong(String[][] revenue,int k, int a) { // k how many lines and a the line of excel where the copy of the other array begin 
         long[][] dataToLong = new long[k][5];
-            for (int i = a - 2 ; i < k + a ; i++) {
+            for (int i = a - 2 ; i < k + a - 2 ; i++) {
                 for (int j = 0 ; j < 5 ; j++) {
-                    dataToLong[i][j] = Long.Parselong(revenue[i][j+2]);
+                    dataToLong[i][j] = Long.parseLong(revenue[i][j+2]);
                 }
             }
+            return dataToLong;
     }
-    long G = converterToLong(revenue, 14, 2); // must be in main
-    long H = converterToLong(revenue, 16, 16); // must be in main
-    long I = converterToLong(revenue, 1, 32); // must be in main
-    public static double[] percentual(long[][] LongData) {
+
+    public static double[][] percentual(long[][] LongData) {
         double[][] variance = new double[32][4];
             for (int i = 0; i < 32; i++) {
                 for (int j = 0; j < 4  ; j++) {
@@ -21,7 +20,7 @@ public class percent {
             }
         return variance;
     }
-    public static double[] amount(long[][] LongData) {
+    public static double[][] amount(long[][] LongData) {
         double[][] amountdiff = new double[LongData.length][4];
             for (int i = 0; i < amountdiff.length ; i++) {
                 for (int j = 0; j < 4  ; j++) {
