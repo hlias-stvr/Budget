75913a436f96691eb44e4e3ea758c4eae191a810
myrtoflorou <myrtoflorou6@gmail.com>
2025-11-19 23:51:58 +0200

    Corrected some mistakes and tested the code successfully

--- Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
@@ -2,26 +2,32 @@ package eurozone.gov.excel;
     
 public class percent {
     public static long[][] converterToLong(String[][] revenue,int k, int a) { // k how many lines and a the line of excel where the copy of the other array begin 
-        long[][] dataToLong = new long[k][5];
-            for (int i = a - 2 ; i < k + a - 2 ; i++) {
+        long[][] LongData = new long[k][5];
+        int i = 0;
+            for (int m = a - 2 ; m < k + a - 2 ; m++) {
                 for (int j = 0 ; j < 5 ; j++) {
-                    dataToLong[i][j] = Long.parseLong(revenue[i][j+2]);
+                    LongData[i][j] = Long.parseLong(revenue[m][j+2]);
                 }
+                i++;
             }
-            return dataToLong;
+            return LongData;
     }
 
     public static double[][] percentual(long[][] LongData) {
-        double[][] variance = new double[32][4];
-            for (int i = 0; i < 32; i++) {
+        double[][] variance = new double [LongData.length][4];
+            for (int i = 0; i < LongData.length; i++) {
                 for (int j = 0; j < 4  ; j++) {
-                    variance[i][j] = ((LongData[i][j] - LongData[i][j+1]) / LongData[i][j+1]) * 100;
+                    if (LongData[i][j+1]!=0) {
+                    variance[i][j] =Math.round(((LongData[i][j] - LongData[i][j+1]) /(double)LongData[i][j+1]) * 100.0*100.0)/100.0;
+                    } else {
+                        variance[i][j] = 0;
+                    }
                 }
             }
         return variance;
     }
-    public static double[][] amount(long[][] LongData) {
-        double[][] amountdiff = new double[LongData.length][4];
+    public static long[][] amount(long[][] LongData) {
+        long[][] amountdiff = new long[LongData.length][4];
             for (int i = 0; i < amountdiff.length ; i++) {
                 for (int j = 0; j < 4  ; j++) {
                     amountdiff[i][j] = (LongData[i][j] - LongData[i][j+1]);
