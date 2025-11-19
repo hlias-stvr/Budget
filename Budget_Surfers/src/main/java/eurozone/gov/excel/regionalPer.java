cd4ba3be0c68f574fa7150063d03a85f6480c1f9
MixalisVerverakis <t8240254@aueb.gr>
2025-11-19 19:12:50 +0200

    Continuation of perPerson and perRegion methods

--- Budget_Surfers/src/main/java/eurozone/gov/excel/regionalPer.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/regionalPer.java
@@ -1,9 +1,14 @@
 package eurozone.gov.excel;
 
 public class regionalPer {
-    public long[] transformToLong(String[][] budget) {
-        long[] budgetLong = new long[7];
-        long[] perReg = new long[7];
+    private long[] budgetLong = new long[7];
+    private long[] transformToLong(String[][] budget) {
+        for (int i = 25 ; i < 32 ; i++) {
+            budgetLong[i - 25] = Long.parseLong(budget[i][2]); 
+        }
+    }
+    public long[] calcBudgetPerPerson() { 
+        double[] perPerson = new double[7];
         long[] population = new long[7];
         population[0] = 3814064;
         population[1] = 1196509;
@@ -12,11 +17,20 @@ public class regionalPer {
         population[4] = 522763;
         population[5] = 624408;
         population[6] = 2357870;
-        for (int i = 25 ; i < 32 ; i++) {
-            budgetLong[i - 25] = Long.parseLong(budget[i][2]); 
-        }
         for (int l = 0 ; l < 7 ; l++) {
-            perReg[l] = (budgetLong[l] / population[l] ) * 100;
+            perPerson[l] = (double) (budgetLong[l] / population[l] ) * 100;
         }
+        return perPerson;
     }
+    public long[] calcBudgetPerRegion() {
+        double[] perRegion = new double[7];
+        int sum = 0;
+        for (int i = 0 ; i < 7 ; i++) {
+            sum = sum + budgetLong[i];
+        }
+        for (int j = 0 ; j < 7 ; j++) {
+            perRegion[j] = (double) (budgetLong[j] / sum) * 100;
+        }
+        return perRegion;
+    } 
 }
