a55e4ea090841fe2da5af67f7e2a65a2b64f0986
Aggelakos23 <aggelostaurinoudis@gmail.com>
2025-12-02 16:56:22 +0200

    Writing initial code in  test class euzlivingstandard

--- Budget_Surfers/src/test/java/eurozone/gov/excel/Testeuzlivingstandard.java
+++ Budget_Surfers/src/test/java/eurozone/gov/excel/Testeuzlivingstandard.java
@@ -5,5 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Testeuzlivingstandard {
-
+    @Test
+    void testcompareToLong() {
+        String[][] gdppop = ReadTwoCsvFiles.readCsv("src\\main\\resourses\\Gdp_population_euz.csv");
+        long[][] testarray2 = EuzLivingStandard.compareToLong(gdppop);
+        assertNotNull(testarray2);
+        assertEquals(19,testarray2.length);
+        assertEquals(2,testarray2[0].length);
+        for (int i = 0 ; i < testarray2.length ; i++) {
+            for (int j = 0 ; j < testarray2[0].length ; j++) {
+                assertTrue(testarray2[i][j] > 0);
+            }
+        }    
+    } 
 }
