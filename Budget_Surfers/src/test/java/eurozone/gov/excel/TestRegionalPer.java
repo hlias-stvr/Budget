c9895289582e152088e8d2eeeaaa54569fbccec6
xrhstosaltanhs <t8240249@aueb.gr>
2025-12-02 20:46:08 +0200

    New test branch for regional percent

---
+++ Budget_Surfers/src/test/java/eurozone/gov/excel/TestRegionalPer.java
@@ -0,0 +1,29 @@
+package eurozone.gov.excel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestRegionalPer {
+    @Test
+    void testTransformToLong {
+        String[][] budget = ReadTwoCsvFiles.readCsv("src\main\resourses\gr_ministy_25.csv");
+        long[] testarray1 = regionalPer.transformToLong(budget);
+        assertNotNull(testarray1);
+        assertEquals(7,testarray1.length);
+        for (int i = 0; i < testarray1.length; i++) {
+            assertTrue(testarray1[i] > 0);
+        }
+    }
+    @Test
+    void testCalcBudgetPerPerson {
+        String[][] budget = ReadTwoCsvFiles.readCsv("src\main\resourses\gr_ministy_25.csv");
+        long[] testarray1 = regionalPer.transformToLong(budget);
+        double[] testarray2 = regionalPer.calcBudgetPerPerson(budget);
+        assertNotNull(testarray2);
+        assertEquals(7,testarray2.length);
+        for (int i = 0; i < testarray2.length; i++) {
+            assertTrue(testarray2[i] > 0);
+        }
+    }
+}    
