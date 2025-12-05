4ea53045acadcd43c99b8f9fb602b7d6059f1dd9
AngelosMel <t8240077@aueb.gr>
2025-12-05 18:47:47 +0200

    Second and final commit of class CompareEuzTaxes

--- Budget_Surfers/src/main/java/eurozone/gov/excel/CompareEuzTaxes.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/CompareEuzTaxes.java
@@ -1,7 +1,8 @@
+package eurozone.gov.excel;
 public class CompareEuzTaxes {
     public static void Calculation(long grt){
         long grtax = grt;
-        long avgeuztax = 248555555556;
+        long avgeuztax = 248555555556L;
         long dif = grtax - avgeuztax;
         if (grtax > avgeuztax) {
             System.out.println("Τα έσοδα που λαμβάνει το ελληνικό κράτος από τη φορολογία (" + grtax + ") ξεπερνούν τον μέσο όρο της ευρωζώνης (" + avgeuztax + ") κατά" + dif);
