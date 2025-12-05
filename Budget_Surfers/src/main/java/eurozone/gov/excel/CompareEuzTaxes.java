5b93a3688b8cb75ae73f579db99b14abcc9d6f2a
AngelosMel <t8240077@aueb.gr>
2025-12-05 19:43:51 +0200

    Added method to compare taxes/gtp with euz

--- Budget_Surfers/src/main/java/eurozone/gov/excel/CompareEuzTaxes.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/CompareEuzTaxes.java
@@ -1,13 +1,16 @@
 package eurozone.gov.excel;
 public class CompareEuzTaxes {
-    public static void Calculation(long grt){
-        long grtax = grt;
-        long avgeuztax = 248555555556L;
-        long dif = grtax - avgeuztax;
+    public static void Calculation(long LongData[][]) {
+        final long GTP = 257100000000L;
+        double grtax = Math.round(((LongData[1][2] + LongData[2][2]) / GTP) * 1000.0) / 1000.0;
+        double avgeuztax = 0.409;
+        double dif = Math.round((grtax - avgeuztax) * 1000.0) / 1000.0;
         if (grtax > avgeuztax) {
-            System.out.println("Τα έσοδα που λαμβάνει το ελληνικό κράτος από τη φορολογία (" + grtax + ") ξεπερνούν τον μέσο όρο της ευρωζώνης (" + avgeuztax + ") κατά" + dif);
+            System.out.println("Τα έσοδα που λαμβάνει το ελληνικό κράτος από τη φορολογία σε ποσοστό δια του ΑΕΠ της Ελλάδος ("
+            + grtax + ") ξεπερνούν εκείνα του μέσου όρου της ευρωζώνης (" + avgeuztax + ") κατά" + dif + " %");
         } else {
-            System.out.println("Τα έσοδα που λαμβάνουν οι χώρες-μέλη της ευρωζώνης κατα μέσο όρο (" + avgeuztax + ") ξεπερνούν τα έσοδα που λαμβάνει το ελληνικό κράτος από τη φορολογία  (" + grtax + ") κατά" + dif);
+            System.out.println("Τα έσοδα που λαμβάνουν κατα μέσο όρο οι χώρες-μέλη της ευρωζώνης από τη φορολογία σε ποσοστό δια του ΑΕΠ ("
+            + avgeuztax + ") ξεπερνούν εκείνα του ελληνικού κράτους (" + grtax + ") κατά" + dif + " %");
         }
     }
 }
