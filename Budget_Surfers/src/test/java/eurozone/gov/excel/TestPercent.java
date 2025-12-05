95db44f8a33d2da3338fc4b373c4ec20f1ae7de6
AngelosMel <t8240077@aueb.gr>
2025-12-05 17:35:28 +0200

    Corrected and tested TestPercent class sucessfully

--- Budget_Surfers/src/test/java/eurozone/gov/excel/TestPercent.java
+++ Budget_Surfers/src/test/java/eurozone/gov/excel/TestPercent.java
@@ -1,9 +1,9 @@
 package eurozone.gov.excel;
-
+ 
 import org.junit.jupiter.api.Test;
-
+ 
 import static org.junit.jupiter.api.Assertions.*;
-
+ 
 public class TestPercent {
     @Test
     void testconvertToLong() {
@@ -13,16 +13,17 @@ public class TestPercent {
         assertNotNull(testarray1);
         assertNotNull(testarray2);
         assertEquals(14,testarray1.length);
-        assertEquals(2,testarray1[0].length);
+        assertEquals(5,testarray1[0].length);
         assertEquals(16,testarray2.length);
-        assertEquals(16,testarray1[0].length);
-        for (int i = 0 ; i < (testarray1.length + testarray2.length) ; i++) {
+        assertEquals(5,testarray1[0].length);
+        for (int i = 0 ; i < testarray1.length; i++) {
             for (int j = 0 ; j < 5 ; j++) {
-                if (i < testarray1.length) {
-                    assertTrue(testarray1[i][j] >= 0);
-                } else {
-                    assertTrue(testarray2[i][j] >= 0);
-                }
+                assertTrue(testarray1[i][j] >= 0);
+            }
+        }
+        for (int i = 0 ; i < testarray2.length; i++) {
+            for (int j = 0 ; j < 5 ; j++) {
+                assertTrue(testarray2[i][j] >= 0);
             }
         }
     }
@@ -36,7 +37,7 @@ public class TestPercent {
         assertEquals(0, testarray3[8][3]);
         double[][] testarray4 = percent.percentual(testarray2);
         assertNotNull(testarray4);
-        assertEquals(0, testarray4[15][2]);
+        assertEquals(0, testarray4[15][1]);
     }
     @Test
     void testamount() {
@@ -51,3 +52,4 @@ public class TestPercent {
         assertEquals(240000, testarray6[5][2]);
     }
 }
+ 
