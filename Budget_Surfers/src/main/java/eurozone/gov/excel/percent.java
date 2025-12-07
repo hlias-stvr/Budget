733e3a7e5f6cf5964653fad45afc00f662eca923
xrhstosaltanhs <t8240249@aueb.gr>
2025-12-07 20:12:28 +0200

    Corrected all the quallity of code in all the Percent class

--- Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/percent.java
@@ -1,7 +1,9 @@
 package eurozone.gov.excel;
     
 public class percent {
-    public static long[][] converterToLong(String[][] revenue,int k, int a) { // k how many lines and a the line of excel where the copy of the other array begin 
+    public static long[][] converterToLong(String[][] revenue,int k, int a) {
+        /*k how many lines and a the line of excel where the
+        copy of the other array begin*/
         long[][] LongData = new long[k][5];
         int i = 0;
             for (int m = a - 2 ; m < k + a - 2 ; m++) {
@@ -12,13 +14,14 @@ public class percent {
             }
             return LongData;
     }
-
     public static double[][] percentual(long[][] LongData) {
         double[][] variance = new double [LongData.length][4];
             for (int i = 0; i < LongData.length; i++) {
                 for (int j = 0; j < 4  ; j++) {
                     if (LongData[i][j+1]!=0) {
-                    variance[i][j] =Math.round(((LongData[i][j] - LongData[i][j+1]) /(double)LongData[i][j+1]) * 100.0*100.0)/100.0;
+                    variance[i][j] = Math.round(((LongData[i][j] -
+                    LongData[i][j+1]) / (double) LongData[i][j+1])
+                    * 100.0 * 100.0) / 100.0;
                     } else {
                         variance[i][j] = 0;
                     }
