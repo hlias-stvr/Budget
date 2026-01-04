e649abd7604a28842726d0cf136fde5ffe5a635b
Ilias Stavrou <t8240247@aueb.gr>
2026-01-04 19:28:44 +0200

    Changed method of asserting true in TestEuzTaxes

--- Budget_Surfers/src/test/java/eurozone/gov/excel/TestEuzTaxes.java
+++ Budget_Surfers/src/test/java/eurozone/gov/excel/TestEuzTaxes.java
@@ -8,10 +8,19 @@ public class TestEuzTaxes {
     @Test 
     void testCalculation() {
         String[][] revenue =
-        ReadTwoCsvFiles.readCsv(
-        "src\main\resourses\gr_revenue_expenses_25.csv");
-        long[][] LongData = percent.converterToLong(revenue, 14, 2);
-        assertNotNull(CompareEuzTaxes.Calculation(LongData));
-        assertFalse(CompareEuzTaxes.Calculation(LongData).isBlank());
+        ReadTwoCsvFiles.readCsv("/gr_revenue_expenses_25.csv");
+        long[][] LongData = Percent.converterToLong(revenue, 14, 2);
+        long[] LongData25 = new long[LongData.length];
+        for (int i = 0; i < LongData.length; i++) {
+            LongData25[i] = LongData[i][0];
+        }
+        final long GTP = 206000000000L;
+        double grtax = Math.round((((LongData25[1] + LongData25[2]) /
+        (double) GTP) * 100) * 10.0) / 10.0;
+        double avgeuztax = Math.round(40.9 * 10.0) / 10.0;
+        double dif = Math.round((Math.abs((grtax - avgeuztax))) * 10.0) / 10.0;
+        assertEquals(grtax, 30.2);
+        assertEquals(avgeuztax, 40.9);
+        assertEquals(dif, 10.7);
     }
 }
