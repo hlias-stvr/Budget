2e08f433b5b8517b27040d1c8ab8d1224cfee6aa
manolisfrai <t8240161@aueb.gr>
2026-01-09 20:48:56 +0200

    Fixed path for reading the csv files in the TestChangeData class

--- Budget_Surfers/src/test/java/eurozone/gov/excel/TestChangeData.java
+++ Budget_Surfers/src/test/java/eurozone/gov/excel/TestChangeData.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestChangeData {
     @Test
     void testNewGrPercent() {
-        String[][] budget = ReadTwoCsvFiles.readCsv("src\main\resourses\gr_ministy_25.csv");
-        long [] A = avgeurozone.convertToLong(budget);
-        double [] grpercent = avgeurozone.ministrDiv(A);
-        String [] grSectors = avgeurozone.sectors();
+        String[][] budget = ReadTwoCsvFiles.readCsv("/gr_ministy_25.csv");
+        long [] A = AvgEurozone.convertToLong(budget);
+        double [] grpercent = AvgEurozone.ministrDiv(A);
+        String [] grSectors = AvgEurozone.sectors();
         double [] testarray = ChangeData.newGrPercent(grpercent, grSectors); 
         assertNotNull(testarray);
         for (int i = 0; i < testarray.length - 1; i++) {
@@ -20,8 +20,8 @@ public class TestChangeData {
     @Test
     void testNewAmountPerRegion() {
         String[][] budget =
-        ReadTwoCsvFiles.readCsv("src\main\resourses\gr_ministy_25.csv");
-        long [] budgetLong = avgeurozone.convertToLong(budget);
+        ReadTwoCsvFiles.readCsv("/gr_ministy_25.csv");
+        long [] budgetLong = AvgEurozone.convertToLong(budget);
         long [] testarray = ChangeData.newAmountPerRegion(budgetLong, budget);
         assertNotNull(testarray);
         for (int i = 0; i < testarray.length - 1; i++) {
@@ -31,9 +31,8 @@ public class TestChangeData {
     @Test
     void testNewRevenue() {
         String[][] revenue =
-        ReadTwoCsvFiles.readCsv(
-        "src\main\resourses\gr_revenue_expenses_25.csv");
-        long[][] LongData = percent.converterToLong(revenue, 14, 2);
+        ReadTwoCsvFiles.readCsv("/gr_revenue_expenses_25.csv");
+        long[][] LongData = Percent.converterToLong(revenue, 14, 2);
         long[] LongData25 = new long[LongData.length];
         for (int i = 0; i < LongData25.length; i++) {
             LongData25[i] = LongData[i][0];
