7b755f54e65fdcbdafb89acfe82855ccfe646eee
xrhstosaltanhs <t8240249@aueb.gr>
2026-01-11 18:28:20 +0200

    Corrected the quality of code in TestAvgEurozone

--- Budget_Surfers/src/test/java/eurozone/gov/excel/TestAvgEurozone.java
+++ Budget_Surfers/src/test/java/eurozone/gov/excel/TestAvgEurozone.java
@@ -1,30 +1,34 @@
 package eurozone.gov.excel;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class TestAvgEurozone {
     @Test
     void testconvertToLong() {
-        String[][] budget = ReadTwoCsvFiles.readCsv("src\\main\\resources\\gr_ministy_25.csv");
+        String[][] budget =
+        ReadTwoCsvFiles.readCsv("src\\main\\resources\\gr_ministy_25.csv");
         long[] testarray1 = AvgEurozone.convertToLong(budget);
         assertNotNull(testarray1);
-        assertEquals(20,testarray1.length);
+        assertEquals(20, testarray1.length);
         for (int i = 0; i < testarray1.length; i++) {
             assertTrue(testarray1[i] > 180000000);
         }
     }
+
     @Test
     void testministrDiv() {
-        String[][] budget = ReadTwoCsvFiles.readCsv("src\\main\\resources\\gr_ministy_25.csv");
+        String[][] budget =
+        ReadTwoCsvFiles.readCsv("src\\main\\resources\\gr_ministy_25.csv");
         long[] testarray1 = AvgEurozone.convertToLong(budget);
         double[] testarray2 = AvgEurozone.ministrDiv(testarray1);
         double sumgrministrpercent = 0.0;
         long sumgrministr = 0;
         final long GTP = 257100000000L;
         assertNotNull(testarray2);
-        assertEquals(11,testarray2.length);
+        assertEquals(11, testarray2.length);
         for (int i = 0; i < testarray2.length - 1; i++) {
             assertTrue(testarray2[i] > 0);
             sumgrministrpercent += testarray2[i];
@@ -33,7 +37,8 @@ public class TestAvgEurozone {
         for (int i = 0; i < testarray1.length; i++) {
             sumgrministr += testarray1[i];
         }
-        double t = Math.round(((double) sumgrministr / GTP * 100)*100.0) / 100.0;
-        assertTrue(Math.abs(t-sumgrministrpercent)< 0.000001);
+        double t =
+            Math.round(((double) sumgrministr / GTP * 100) * 100.0) / 100.0;
+        assertTrue(Math.abs(t - sumgrministrpercent) < 0.000001);
     }
 }
