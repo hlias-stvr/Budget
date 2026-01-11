a6fe4561ad45d709e28897bf31f4821118c7cf29
xrhstosaltanhs <t8240249@aueb.gr>
2026-01-11 18:42:10 +0200

    Corrected the quality of code in TestEuzLivingStandard

--- Budget_Surfers/src/test/java/eurozone/gov/excel/TestEuzLivingStandard.java
+++ Budget_Surfers/src/test/java/eurozone/gov/excel/TestEuzLivingStandard.java
@@ -1,7 +1,10 @@
 package eurozone.gov.excel;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 public class TestEuzLivingStandard {
     @Test
     void testcompareToLong() {
@@ -9,24 +12,25 @@ public class TestEuzLivingStandard {
         ReadTwoCsvFiles.readCsv("/Gdp_population_euz.csv");
         long[][] testarray2 = EuzLivingStandard.compareToLong(gdppop);
         assertNotNull(testarray2);
-        assertEquals(19,testarray2.length);
-        assertEquals(2,testarray2[0].length);
-        for (int i = 0 ; i < testarray2.length ; i++) {
-            for (int j = 0 ; j < testarray2[0].length ; j++) {
+        assertEquals(19, testarray2.length);
+        assertEquals(2, testarray2[0].length);
+        for (int i = 0; i < testarray2.length; i++) {
+            for (int j = 0; j < testarray2[0].length; j++) {
                 assertTrue(testarray2[i][j] > 0);
             }
         }
     }
+
     @Test
     void testfindStandLiving() {
         String[][] gdppop = ReadTwoCsvFiles.readCsv("/Gdp_population_euz.csv");
         long[][] testarray2 = EuzLivingStandard.compareToLong(gdppop);
         double[] testarray3 = EuzLivingStandard.findStandLiving(testarray2);
         assertNotNull(testarray2);
-        assertEquals(19,testarray2.length);
+        assertEquals(19, testarray2.length);
         assertNotNull(testarray3);
-        assertEquals(20,testarray3.length);
-        for (int j = 0 ; j < testarray2.length ; j++) {
+        assertEquals(20, testarray3.length);
+        for (int j = 0; j < testarray2.length; j++) {
             assertTrue(testarray2[j][1] > 560000);
             assertTrue(testarray2[j][0] > 7703840000L);
             assertTrue(testarray3[j] > 10000);
