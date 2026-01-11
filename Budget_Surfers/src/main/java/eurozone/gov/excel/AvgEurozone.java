2b0fe9f4bf7b7b71dc3f6c9af5e83893ea64d850
manolisfrai <t8240161@aueb.gr>
2026-01-11 12:46:31 +0200

    Enden correcting quality of code in AvgEurozone class

--- Budget_Surfers/src/main/java/eurozone/gov/excel/AvgEurozone.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/AvgEurozone.java
@@ -34,7 +34,7 @@ public class AvgEurozone {
         double [] grpercent = new double[11];
         for (int i = 0; i < 11; i++) {
             grpercent[i] = Math.round(((double) sectors[i] / GTP * 100)
-                *100.0) / 100.0;
+                * 100.0) / 100.0;
         }
         return grpercent;
     }
@@ -48,10 +48,11 @@ public class AvgEurozone {
 
     public static double [] compareGrToEurozone(double[] grpercent) {
         double [] euzpercent = {6.0, 1.2, 1.7, 5.7, 0.9, 1.2,
-             7.4, 1.1, 4.6, 19.8, 49.5};
+            7.4, 1.1, 4.6, 19.8, 49.5};
         double [] diffgreuz = new double[11];
-        for(int i = 0; i<11; i++) {
-            diffgreuz[i] = Math.round((euzpercent[i] - grpercent[i]) * 100.0) /100.0;
+        for (int i = 0; i < 11; i++) {
+            diffgreuz[i] = Math.round((euzpercent[i] - grpercent[i])
+                * 100.0) / 100.0;
         }
         return diffgreuz;
     }
