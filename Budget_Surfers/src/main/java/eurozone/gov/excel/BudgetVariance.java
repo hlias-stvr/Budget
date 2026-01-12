b252c387e7d2813328b0be16c8a5c91f5454cda0
Aggelakos23 <aggelostaurinoudis@gmail.com>
2026-01-12 15:24:33 +0200

    style of code in BudgetVariance

--- Budget_Surfers/src/main/java/eurozone/gov/excel/BudgetVariance.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/BudgetVariance.java
@@ -6,11 +6,11 @@ public class BudgetVariance {
         copy of the other array begin*/
         long[][] longData = new long[k][5];
         int i = 0;
-        for (int m = a - 2 ; m < k + a - 2 ; m++) {
-            for (int j = 0 ; j < 5 ; j++) {
-                longData[i][j] = Long.parseLong(revenue[m][j+2]);
+        for (int m = a - 2; m < k + a - 2; m++) {
+            for (int j = 0; j < 5; j++) {
+                longData[i][j] = Long.parseLong(revenue[m][j + 2]);
             }
-        i++;
+            i++;
         }
         return longData;
     }
@@ -25,19 +25,19 @@ public class BudgetVariance {
                     longData[i][j + 1]) / (double) longData[i][j + 1])
                     * 100.0 * 100.0) / 100.0;
                 } else {
-                percVariance[i][j] = 0;
+                    percVariance[i][j] = 0;
                 }
             }
         }
-    return percVariance;
+        return percVariance;
     }
 
     //Ποσοτικές μεταβολές εσόδων ή εξόδων ανά έτη
     public static long[][] amount(long[][] longData) {
         long[][] amountVariance = new long[longData.length][4];
-        for (int i = 0; i < amountVariance.length ; i++) {
-            for (int j = 0; j < 4  ; j++) {
-                amountVariance[i][j] = (longData[i][j] - longData[i][j+1]);
+        for (int i = 0; i < amountVariance.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                amountVariance[i][j] = (longData[i][j] - longData[i][j + 1]);
             }
         }
         return amountVariance;
