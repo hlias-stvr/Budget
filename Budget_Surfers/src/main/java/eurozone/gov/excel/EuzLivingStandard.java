53374f6dbdf3236f4b6e5cbdc57fb57ad687eed3
Aggelakos23 <aggelostaurinoudis@gmail.com>
2026-01-12 15:50:02 +0200

    style improvement of EuzLivingStandard

--- Budget_Surfers/src/main/java/eurozone/gov/excel/EuzLivingStandard.java
+++ Budget_Surfers/src/main/java/eurozone/gov/excel/EuzLivingStandard.java
@@ -4,20 +4,21 @@ public class EuzLivingStandard {
     //Mετατροπή πίνακα gdpPop σε long
     public static long[][] compareToLong(String[][] gdpPop) {
         long[][] longGdpPop = new long[19][2];
-        for (int i = 0 ; i < 19 ; i++) {
-            for (int j = 0 ; j < 2 ; j++) {
-                longGdpPop[i][j] = Long.parseLong(gdpPop[i][j+1]);
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 2; j++) {
+                longGdpPop[i][j] = Long.parseLong(gdpPop[i][j + 1]);
             }
         }
         return longGdpPop;
     }
+
     public static final int GRAVGGDP = 25300;
 
     //Δημιουργεί πίνακα με τα κατά κεφαλήν ΑΕΠ όλων των χωρών της Ευρωζώνης
     public static double[] findStandLiving(long[][] longGdpPop) {
         double[] euzGdpPerCapita = new double[20];
         double sumLivingStd = 0;
-        for (int k = 0 ; k < 19 ; k++) {
+        for (int k = 0; k < 19; k++) {
             euzGdpPerCapita[k] = (double) (longGdpPop[k][0] / longGdpPop[k][1]);
             sumLivingStd = sumLivingStd + euzGdpPerCapita[k];
         }
@@ -29,31 +30,35 @@ public class EuzLivingStandard {
     *Συγκρίνει το κατά κεφαλήν ΑΕΠ(ως δείκτη του βιοτικού επιπέδου)
     *της Ελλάδας με αυτό κάποιας επιλεγμένης χώρας
     */
-    public static void compareStdLive(int a, double[] euzStdLiv, String[][] gdpPop) {
+    public static void compareStdLive(int a,
+        double[] euzStdLiv,
+        String[][] gdpPop) {
         if (a != 20) {
             double stdLiveDiff = euzStdLiv[a - 1] - GRAVGGDP; /* a - 1 because
             we want user's coices to start from number 1 */
             if (stdLiveDiff > 0) {
-                System.out.println("Η " + gdpPop[a-1][0] + " έχει μεγαλύτερο " +
+                System.out.println("Η " + gdpPop[a - 1][0] + " έχει μεγαλύτερο " +
                 "βιοτικό επίπεδο από την Ελλάδα κατά " + stdLiveDiff +
                 " μονάδες του ευρώ.");
             } else if (stdLiveDiff < 0) {
-                System.out.println("Η " + gdpPop[a-1][0] + " έχει μικρότερο βιοτικό"
-                + " επίπεδο από την Ελλάδα κατά " + Math.abs(stdLiveDiff) +
+                System.out.println("Η " + gdpPop[a - 1][0] +
+                " έχει μικρότερο βιοτικό" +
+                " επίπεδο από την Ελλάδα κατά " + Math.abs(stdLiveDiff) +
                 " μονάδες του ευρώ.");
             } else {
-                System.out.println("Η " + gdpPop[a-1][0] +
+                System.out.println("Η " + gdpPop[a - 1][0] +
                 " έχει το ίδιο βιοτικό επίπεδο με την Ελλάδα.");
             }
         } else {
-            double stdLiveGrEuz = Math.round((euzStdLiv[19] - GRAVGGDP) * 10.0) / 10.0;
+            double stdLiveGrEuz = Math.round((euzStdLiv[19] - GRAVGGDP) * 10.0)
+            / 10.0;
             if (stdLiveGrEuz > 0) {
-                System.out.println("Η Ελλάδα έχει μικρότερο βιοτικό επίπεδο από " +
-                "τον μέσο όρο της Ευρωζώνης κατά " + stdLiveGrEuz +
+                System.out.println("Η Ελλάδα έχει μικρότερο βιοτικό επίπεδο" +
+                " από τον μέσο όρο της Ευρωζώνης κατά " + stdLiveGrEuz +
                 " μονάδες του ευρώ.");
             } else if (stdLiveGrEuz < 0) {
-                System.out.println("Η Ελλάδα έχει μεγαλύτερο βιοτικό επίπεδο από" +
-                " τον μέσο όρο της Ευρωζώνης κατά " + Math.abs(stdLiveGrEuz) +
+                System.out.println("Η Ελλάδα έχει μεγαλύτερο βιοτικό επίπεδο" +
+                " από τον μέσο όρο της Ευρωζώνης κατά " + Math.abs(stdLiveGrEuz) +
                 " μονάδες του ευρώ.");
             } else {
                 System.out.println("Η Ελλάδα έχει το ίδιο βιοτικό επίπεδο " +
